ad6ad01478ecddf67414777e9866b1ccc994f8cb
2puggs <mirabishh@gmail.com>
2025-08-01 16:10:54 -0500

    completed example with lifecycle hook onMounted() call

---
+++ Vue/test/test-vue/RUN.md
@@ -0,0 +1,14 @@
+
+# Notes on Vue/ What I learned 
+
+### how to run: 
+
+`cd test-vue `
+`npm install `
+`npm run dev `
+
+###### Optional: Initialize git in your project directory with 
+
+` git init && id add -A git commit -m "initial commit" `
+
+###### Composition Style is the preferred style 
--- Vue/test/test-vue/src/App.vue
+++ Vue/test/test-vue/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue';
+import { onMounted } from 'vue';
 //Composition API style
 
     const name = ref('miranda b');
     const status = ref('active');
     const tasks = ref(['task1', 'task2', 'task3']);
+    const newTask = ref('');
 
     const toggleStatus = () => { //need status to be reactive ref() -> reactive components don't use keywork they use value
       if (status.value === 'active') {
@@ -16,6 +18,29 @@ import { ref } from 'vue';
         status.value = 'active';
       }
     };
+
+    const addTask = () => {
+      //validation
+      if (newTask.value.trim() !== '') {
+        tasks.value.push(newTask.value);
+        newTask.value = '';
+      }
+
+    }
+
+    const deleteTask = (index) => {
+      tasks.value.splice(index,1);
+    }
+
+    onMounted(async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        tasks.value = data.map((task) => task.title);
+      } catch (error) {
+        console.log('Error fetching tasks');
+      }
+    });
 </script>
 <template>
   <h1> {{ name }} </h1>
@@ -24,9 +49,21 @@ import { ref } from 'vue';
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inacitve</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask"></input> 
+    <button type="submit">Submit</button>
+     <!-- can bind data to input v-model directive-->
+  </form>
   <h3> Tasks </h3>
   <ul>
-      <li v-for="task in tasks" :key="task">{{ task }}</li>
+      <li v-for="(task,index) in tasks" :key="task">
+        <span>
+        {{ task }}
+        </span>
+        <button @click="deleteTask(index)">X</button>
+      </li>
+
   </ul>
   <br />
   <button @click="toggleStatus">Change Status</button> 
