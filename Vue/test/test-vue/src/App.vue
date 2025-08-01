b4e8bdb74238845f39c4ad8d53eaba0a5ea2d08b
2puggs <mirabishh@gmail.com>
2025-08-01 14:19:10 -0500

    test written in Composition style

--- Vue/test/test-vue/src/App.vue
+++ Vue/test/test-vue/src/App.vue
@@ -1,26 +1,21 @@
-<script>
-export default {
-  data () {
-    return {
-      name: 'miranda b',
-      status: 'active',
-      tasks: ['task1', 'task2', 'task3'],
-      link: 'https://google.com', 
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending';
+<script setup>
+import { ref } from 'vue';
+//Composition API style
+
+    const name = ref('miranda b');
+    const status = ref('active');
+    const tasks = ref(['task1', 'task2', 'task3']);
+
+    const toggleStatus = () => { //need status to be reactive ref() -> reactive components don't use keywork they use value
+      if (status.value === 'active') {
+        status.value = 'pending';
       }
-      else if (this.status === 'pending') {
-        this.status ='inactive';
+      else if (status.value === 'pending') {
+        status.value ='inactive';
       } else {
-        this.status = 'active';
+        status.value = 'active';
       }
-    },
-  }
-};
+    };
 </script>
 <template>
   <h1> {{ name }} </h1>
@@ -29,15 +24,12 @@ export default {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inacitve</p>
 
-  <h3>
+  <h3> Tasks </h3>
   <ul>
       <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <a :href="link">goto google</a>
   <br />
   <button @click="toggleStatus">Change Status</button> 
-  <!--<a v-bind:href="link">Click</a>-->
-  </h3>
 </template>
 
 <style scoped>
