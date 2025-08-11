1db544eacb2585d4c20d14d8335e91ae7af969f6
2puggs <mirabishh@gmail.com>
2025-08-10 21:57:06 -0500

    understanding and implementing router links to link pages and b uttons together, replaced href and <a> tags

--- Vue/test/test-vue/src/router/index.js
+++ Vue/test/test-vue/src/router/index.js
@@ -1,12 +1,18 @@
 // main router file to add about page set home page to home vue
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import JobsView from '@/views/JobsView.vue';
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: HomeView,
-    }
+    },
+    {
+        path: '/jobs',
+        name: 'Jobs',
+        component: JobsView,
+    },
 ];
 const router = createRouter({
     history: createWebHistory(),
